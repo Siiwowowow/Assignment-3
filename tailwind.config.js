0d53667b32ff151f04085eb17eb2283e283fdf56
Siiwowowow <avasiaa32290@gmail.com>
2025-02-01 13:45:50 +0600

    Image added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        animation:{
+          wiggle:"wiggle 4s ease-in-out infinite",
+        },
+        keyframes: {
+          wiggle:{
+            "0% 100%":{
+              transform:"rotate(-10deg)",
+            },
+            "50%":{
+              transform:"rotate(30deg)",
+            },
+          },
+        },
+      },
     },
     plugins: [],
-  }
+  };
